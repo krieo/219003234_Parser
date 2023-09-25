e5fed59e1f7644416e1748ffd1017498fee4d608
Krieo <krieolin@gmail.com>
2023-09-25 12:21:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _219003234_Parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
