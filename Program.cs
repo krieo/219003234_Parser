d00ae8f9e04fc60ed20961b41141297fe3e89ae9
Krieo <krieolin@gmail.com>
2023-09-25 12:54:45 +0200

    made a basic calculator

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
 
 namespace _219003234_Parser
 {
@@ -6,7 +8,16 @@ namespace _219003234_Parser
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter an expression: ");
+            string input = Console.ReadLine();
+
+            ICharStream stream = new AntlrInputStream(input);
+            SimpleCalcLexer lexer = new SimpleCalcLexer(stream);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            SimpleCalcParser parser = new SimpleCalcParser(tokens);
+
+            IParseTree tree = parser.start();
+            Console.WriteLine("Parse tree: " + tree.ToStringTree(parser));
         }
     }
 }
